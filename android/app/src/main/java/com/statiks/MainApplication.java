8cf5d5f3917e953e76e34f5568de18a55ff94419
Jérémy Barbet <jeremgraph@gmail.com>
2017-12-20 11:34:03 +0000

    Android build

--- android/app/src/main/java/com/statiks/MainApplication.java
+++ android/app/src/main/java/com/statiks/MainApplication.java
@@ -1,42 +1,36 @@
 package com.statiks;
 
-import android.app.Application;
-
-import com.facebook.react.ReactApplication;
-import com.reactnativenavigation.NavigationReactPackage;
-import com.facebook.react.ReactNativeHost;
+import com.reactnativenavigation.NavigationApplication;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new NavigationReactPackage()
-      );
-    }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
+public class MainApplication extends NavigationApplication {
 
   @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+  public boolean isDebug() {
+    // Make sure you are using BuildConfig from your own application
+    return BuildConfig.DEBUG;
+  }
+
+  protected List<ReactPackage> getPackages() {
+    // Add additional packages you require here
+    // No need to add RnnPackage and MainReactPackage
+    return Arrays.<ReactPackage>asList(
+
+    );
+  }
+
+  @Override
+  public String getJSMainModuleName() {
+    return "index";
+  }
+
+  @Override
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return getPackages();
   }
 
   @Override

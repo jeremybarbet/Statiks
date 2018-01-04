be3084e90b172ba3e95a8d90c1eb1fc8d19a7c61
Jérémy Barbet <jeremgraph@gmail.com>
2018-01-04 11:23:18 +0000

    Add splash screen for android

--- android/app/src/main/java/com/statiks/MainActivity.java
+++ android/app/src/main/java/com/statiks/MainActivity.java
@@ -1,5 +1,31 @@
 package com.statiks;
 
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.ImageView;
+import android.view.Gravity;
+import android.util.TypedValue;
+import android.support.v4.content.ContextCompat;
+
 import com.reactnativenavigation.controllers.SplashActivity;
 
-public class MainActivity extends SplashActivity {}
+public class MainActivity extends SplashActivity {
+
+    @Override
+    public LinearLayout createSplashLayout() {
+        LinearLayout view = new LinearLayout(this);
+        ImageView imageView = new ImageView(this);
+
+        view.setBackgroundColor(Color.parseColor("#f0f6fa"));
+        view.setGravity(Gravity.CENTER);
+
+        // hard code the width and the height of the logo
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(432, 340);
+        layoutParams.gravity = Gravity.CENTER;
+        imageView.setLayoutParams(layoutParams);
+        imageView.setImageDrawable(ContextCompat.getDrawable(this.getApplicationContext(), R.drawable.logo_file));
+
+        view.addView(imageView);
+        return view;
+    }
+}
